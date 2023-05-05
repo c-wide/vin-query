3c4ad129b87682fefcc2d2c1fd9b936205c21846
c-wide <53365073+c-wide@users.noreply.github.com>
2023-05-05 08:07:14 -0500

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { SunIcon, MoonIcon } from "@heroicons/react/20/solid";
 import CustomTable from "./CustomTable";
 import { useDarkMode } from "./hooks/useDarkMode";
@@ -19,35 +20,38 @@ export default function App() {
   };
 
   return (
-    <div
-      className={`min-h-screen transition-colors ${
-        transitionDuration === 0 ? "" : "duration-500 ease-in-out"
-      } bg-gray-100 dark:bg-gray-800`}
-    >
-      <div className="py-6 px-6">
-        <div className="grid grid-cols-3">
-          <div></div>
-          <div className="flex items-center justify-center">
-            <img
-              className="h-[100px]"
-              src={darkMode ? darkLogo : lightLogo}
-              alt="logo"
-            />
+    <>
+      <div
+        className={`min-h-screen transition-colors ${
+          transitionDuration === 0 ? "" : "duration-500 ease-in-out"
+        } bg-gray-100 dark:bg-gray-800`}
+      >
+        <div className="py-6 px-6">
+          <div className="grid grid-cols-3">
+            <div></div>
+            <div className="flex items-center justify-center">
+              <img
+                className="h-[100px]"
+                src={darkMode ? darkLogo : lightLogo}
+                alt="logo"
+              />
+            </div>
+            <div className="flex items-center justify-end">
+              <button onClick={handleClick} className="focus:outline-none">
+                {darkMode ? (
+                  <MoonIcon className="w-6 h-6 text-gray-500" />
+                ) : (
+                  <SunIcon className="w-6 h-6 text-yellow-500" />
+                )}
+              </button>
+            </div>
           </div>
-          <div className="flex items-center justify-end">
-            <button onClick={handleClick} className="focus:outline-none">
-              {darkMode ? (
-                <MoonIcon className="w-6 h-6 text-gray-500" />
-              ) : (
-                <SunIcon className="w-6 h-6 text-yellow-500" />
-              )}
-            </button>
+          <div className="mt-8">
+            <CustomTable />
           </div>
-        </div>
-        <div className="mt-8">
-          <CustomTable />
         </div>
       </div>
-    </div>
+      <Analytics />
+    </>
   );
 }
